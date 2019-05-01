f272b178e6759217c1ede0512f09b6bc224bdbf6
David Wukelic <david.wukelic@gmail.com>
2019-04-30 21:37:06 -0700

    added basic property binding and directives using ngStyle & ngClass

--- Sandbox/src/app/home/home.component.ts
+++ Sandbox/src/app/home/home.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+    toggleOn: boolean = false;
+    counter = 0;
+    clicks = [];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    findColor(num){
+        if (num >= 5){
+            return 'blue';
+        } else{
+            console.log(num)
+        }
+    }
+
+    onClick(){
+        this.counter += 1;
+        this.clicks.push(this.counter)
+        this.toggleOn = !(this.toggleOn)
+    }
 
 }
